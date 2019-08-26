6330578d6eabf3f3d375d0b1846b3df484861aff
semivori <semivoriii@gmail.com>
2019-08-26 22:04:24 +0300

    add build

---
+++ build/precache-manifest.3c54b31ff31b016ae0c87923bb7dc4ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "968a1061ab3f009e969ab12b4022ea93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc4d63aba24f57cfd1c0",
+    "url": "/static/css/main.f63aa0ae.chunk.css"
+  },
+  {
+    "revision": "9319774a87fb395a0206",
+    "url": "/static/js/2.d6ab339c.chunk.js"
+  },
+  {
+    "revision": "cc4d63aba24f57cfd1c0",
+    "url": "/static/js/main.efffebad.chunk.js"
+  },
+  {
+    "revision": "8277e3460bb72fd0789b",
+    "url": "/static/js/runtime~main.240aadcb.js"
+  }
+]);
